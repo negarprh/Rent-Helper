348751fe05e23bc4b2668ae57ac80ee7894e762c
Negar <negarpr@hotmail.com>
2026-03-05 17:14:10 -0500

    new changes

---
+++ RentHelper/Views/Auth/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  RentHelper
+//
+//  Created by Naomi on 2026-03-02.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
